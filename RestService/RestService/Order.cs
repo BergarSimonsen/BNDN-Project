ffabba7bacdff1ad5fe894cd2488e72d4e5e1ee5
Alexander Kirk Joergensen <alkii900@msn.com>
2013-03-29 03:29:08 +0100

    Order added

---
+++ RestService/RestService/Order.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace RestService
+{
+    [DataContract]
+    public class Order
+    {
+        [DataMember]
+        public int id;
+
+        [DataMember]
+        public string title;
+
+        [DataMember]
+        public int user_id;
+
+        [DataMember]
+        public string status;
+
+        public Order(int id, string title, int user_id, string status)
+        {
+            this.id = id;
+            this.title = title;
+            this.user_id = user_id;
+            this.status = status;
+        }
+    }
+}
