4f3a81c660a7bf146ca59f7ef063471c5f4138b3
Morten Holm Hvass <akkri777@gmail.com>
2013-04-02 09:56:10 +0200

    DB-con added

---
+++ RestService/RestService/DatabaseConnector.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ConsoleApplication1
+{
+    class DatabaseConnector
+    {
+        
+        static void Main(string[] args)
+        {
+            DatabaseConnector dbCon = DatabaseConnector.GetInstance;
+            //dbCon.Connect("ITU");
+            //dbCon.CloseConnection();
+            //dbCon.Connect("SMU");
+            //dbCon.CloseConnection();
+            dbCon.selectTest();
+            Console.ReadKey();
+        }
+
+        private static DatabaseConnector instance;
+        private SqlConnection connection;
+        private string connectionString;
+        private Dictionary<string, string> databases;
+
+        public static DatabaseConnector GetInstance
+        {
+            get
+            {
+                if (instance == null) instance = new DatabaseConnector();
+                return instance;
+            }
+        }
+
+        private DatabaseConnector()
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        { 
+            //load info about databases and apps into dictionary here
+            databases = new Dictionary<string,string>();
+            databases.Add("ITU", "ItuDatabase");
+            databases.Add("SMU","SmuDatabase");
+        }
+
+        private SqlConnection Connect(string s)
+        { 
+            connectionString = "Server=rentit.itu.dk;DATABASE="+databases[s]+";UID=Rentit26db;PASSWORD=ZAQ12wsx;";
+            connection = new SqlConnection(connectionString);
+            try
+            {
+                if (connection.State != System.Data.ConnectionState.Open) connection.Open();
+                Console.WriteLine("works");
+                return connection;
+            }
+            catch(SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+                return null;
+            }
+            
+        }
+
+        public bool CloseConnection()
+        {
+            if (connection.State != System.Data.ConnectionState.Closed) { connection.Close(); return true; }
+            else return false;
+        }
+
+        private void ExecuteQuery(string query, string database)
+        {
+            if (connection.State != System.Data.ConnectionState.Open) Connect(database);
+            if (connection.State != System.Data.ConnectionState.Open) ErrorMessage("Cannot open connection to server");
+            else
+            {
+                SqlCommand cmd = new SqlCommand(query, connection);
+                cmd.ExecuteNonQuery();
+                CloseConnection();
+            }
+        }
+
+        private SqlDataReader ExecuteReader(string query, string database)
+        {
+            if (connection.State != System.Data.ConnectionState.Open) Connect(database);
+            if (connection.State != System.Data.ConnectionState.Open) ErrorMessage("Cannot open connection to server");
+            else
+            {
+                try
+                {
+                    SqlCommand cmd = new SqlCommand(query, connection);
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    //CloseConnection();
+                    return reader;
+                }
+                catch (SqlException e) { ErrorMessage(e.ToString()); }
+            }
+            return null;
+        }
+
+        private void ErrorMessage(string s)
+        {
+            Console.Write(s);
+        }
+
+//******************************************************** User *******************************************************
+        public void selectTest()
+        {
+            Connect("SMU");
+            string query = "insert into user_account_data_type (name) values('kaboom')";
+            ExecuteQuery(query, "SmuDatabase");
+            CloseConnection();
+        }
+    }
+}
