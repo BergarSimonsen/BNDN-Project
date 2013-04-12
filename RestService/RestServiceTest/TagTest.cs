408641cacac9e63ddedce7ec89bdb4fcf244b76b
Bergar Simonsen <bergar.simonsen@gmail.com>
2013-04-12 17:07:42 +0200

    added mediaTest, tagTest, userTest files

---
+++ RestService/RestServiceTest/TagTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RestServiceTest
+{
+    [TestClass]
+    public class TagTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
