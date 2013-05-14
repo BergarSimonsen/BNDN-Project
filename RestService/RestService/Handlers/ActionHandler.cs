570af3e45e1423baf74b4f292820d207159fc955
Morten Holm Hvass <akkri777@gmail.com>
2013-05-14 15:37:57 +0200

    asd

--- RestService/RestService/Handlers/ActionHandler.cs
+++ RestService/RestService/Handlers/ActionHandler.cs
@@ -9,45 +9,45 @@ using RestService.Entities;
 
 namespace RestService.Handlers
 {
-    class UserGroupHandler : AbstractHandler<UserGroup>
+    class ActionHandler : AbstractHandler<RestService.Entities.Action>
     {
-        public UserGroupHandler(DatabaseConnection incDbCon, Permissions perm) : base(incDbCon, perm) { }
+        public ActionHandler(DatabaseConnection incDbCon, Permissions perm) : base(incDbCon, perm) { }
 
         public override void Create(Dictionary<string, string> data)
         {
             Validate(data);
 
-            PreparedStatement stat = dbCon.Prepare("INSERT INTO user_group (name, description) VALUES (@name, @description)"
+            PreparedStatement stat = dbCon.Prepare("INSERT INTO action (name, description) VALUES (@name, @description)"
             , new List<string> {"name", "description"});
             
             dbCon.Command(data, stat);
         }
 
-        public override UserGroup[] Read(int id)
+        public override RestService.Entities.Action[] Read(int id)
         {
-            PreparedStatement stat = dbCon.Prepare("SELECT * FROM user_group where id = '" + id + "'", 
+            PreparedStatement stat = dbCon.Prepare("SELECT * FROM action where id = '" + id + "'", 
             new List<string> { });
 
-            return ListToArray(CreateUserGroup(dbCon.Query(new Dictionary<string, string>(), stat)));
+            return ListToArray(CreateAction(dbCon.Query(new Dictionary<string, string>(), stat)));
         }
 
         public override void Update(int id, Dictionary<string, string> data)
         {
             Validate(data);
 
-            PreparedStatement stat = dbCon.Prepare("UPDATE user_group (name, description)" +
+            PreparedStatement stat = dbCon.Prepare("UPDATE action (name, description)" +
             "VALUES (@name, @description)", new List<string> { "name", "description"});
             dbCon.Command(data, stat);
         }
 
         public override void Delete(int id)
         {
-            PreparedStatement stat = dbCon.Prepare("DELETE FROM user_group where id = '"+id+"'",new List<string>());
+            PreparedStatement stat = dbCon.Prepare("DELETE FROM action where id = '"+id+"'",new List<string>());
 
             dbCon.Command(new Dictionary<string, string>(), stat);
         }
 
-        public override UserGroup[] Search(Dictionary<string, string> data) 
+        public override RestService.Entities.Action[] Search(Dictionary<string, string> data) 
         {
             string searchParams = "";
             
@@ -62,37 +62,38 @@ namespace RestService.Handlers
             // removes the last "and" since there are no more params to search for
             searchParams.Remove(searchParams.Length - 4);
 
-            PreparedStatement stat = dbCon.Prepare("SELECT * FROM user_group where " + searchParams, list);
+            PreparedStatement stat = dbCon.Prepare("SELECT * FROM action where " + searchParams, list);
 
-            return ListToArray(CreateUserGroup(dbCon.Query(data, stat)));
+            return ListToArray(CreateAction(dbCon.Query(data, stat)));
         }
 
         
         public override void Validate(Dictionary<string, string> data)
         {
             if (!data.ContainsKey("name"))
-                throw new Exception("UserGroup is missing 'name' data");
+                throw new Exception("Action is missing 'name' data");
             if (!data.ContainsKey("description"))
-                throw new Exception("UserGroup is missing 'description' data");
+                throw new Exception("Action is missing 'description' data");
         }
 
-        private List<UserGroup> CreateUserGroup(SqlDataReader reader)
+        private List<RestService.Entities.Action> CreateAction(SqlDataReader reader)
         {
-            List<UserGroup> returnUserGroups = new List<UserGroup>();
+            List<RestService.Entities.Action> returnActions = new List<RestService.Entities.Action>();
 
             while (reader.Read())
             { 
                 int id = reader.GetInt32(reader.GetOrdinal("id"));
                 string name = reader.GetString(reader.GetOrdinal("name"));
                 string description= reader.GetString(reader.GetOrdinal("description"));
+                bool allowed= reader.GetBoolean(reader.GetOrdinal("allowed"));
 
-                returnUserGroups.Add(new UserGroup(id, name, description));
+                returnActions.Add(new RestService.Entities.Action(id, name, description, allowed));
             }
 
-            return returnUserGroups;
+            return returnActions;
         }
 
-        private UserGroup[] ListToArray(List<UserGroup> incList)
+        private RestService.Entities.Action[] ListToArray(List<RestService.Entities.Action> incList)
         {
             return incList.ToArray();
         }
