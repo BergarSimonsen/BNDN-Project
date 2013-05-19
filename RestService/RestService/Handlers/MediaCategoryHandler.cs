ec7dce5e9c66edbb2a75dd1742c367dd52bc106a
unknown <akkri777@gmial.com>
2013-05-19 14:24:05 +0200

    all handlers done

---
+++ RestService/RestService/Handlers/MediaCategoryHandler.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using RestService.Security;
+using RestService.Entities;
+
+namespace RestService.Handlers
+{
+    class MediaCategoryHandler : AbstractHandler<MediaCategory>
+    {
+        public MediaCategoryHandler(DatabaseConnection incDbCon, Permissions perm) : base(incDbCon, perm) { }
+
+        public override void Create(Dictionary<string, string> data)
+        {
+            Validate(data);
+
+            PreparedStatement stat = dbCon.Prepare("INSERT INTO media_category (name, description) VALUES ('" + data["name"] + "', '" + data["description"] + "')");
+            
+            dbCon.Command(data, stat);
+        }
+
+        public override MediaCategory[] Read(int id)
+        {
+            PreparedStatement stat = dbCon.Prepare("SELECT * FROM media_category where id = '" + id + "'");
+
+            return ListToArray(CreateMediaCategory(dbCon.Query(new Dictionary<string,string>(), stat)));
+        }
+
+        public override void Update(int id, Dictionary<string, string> data)
+        {
+            Validate(data);
+
+            DateTime now = DateTime.Now;
+
+            data.Add("modified", now.Year + "-" + now.Month + "-" + now.Day + " " + now.Hour + ":" + now.Minute + ":" + now.Second);
+
+            PreparedStatement stat = dbCon.Prepare("UPDATE media_category SET email = '"+data["email"]+"', password_hash = '"+data["password"]+"', modified = '"+data["modified"]+"') " +
+            "WHERE id = '"+id+"'");
+            dbCon.Command(data, stat);
+        }
+
+        public override void Delete(int id)
+        {
+            PreparedStatement stat = dbCon.Prepare("DELETE FROM media_category where id = '"+id+"'");
+
+            dbCon.Command(new Dictionary<string, string>(), stat);
+        }
+
+        public override MediaCategory[] Search(Dictionary<string, string> data) 
+        {
+            string searchParams = "";
+
+            if (data.Count != 0)
+            {
+                searchParams += " where ";
+
+                foreach (KeyValuePair<string, string> s in data)
+                {
+                    string semiResult = s.Key + " = '" + s.Value + "' and ";
+                    searchParams += semiResult;
+                }
+
+                // removes the last "and" since there are no more params to search for
+                searchParams = searchParams.Remove(searchParams.Length - 4);
+            }
+
+            PreparedStatement stat = dbCon.Prepare("SELECT * FROM media_category"+ searchParams);
+
+            return ListToArray(CreateMediaCategory(dbCon.Query(data, stat)));
+        }
+        
+        public override void Validate(Dictionary<string, string> data)
+        {
+            if (!data.ContainsKey("name"))
+                throw new Exception("MediaCategory is missing 'name' data");
+            if (!data.ContainsKey("description"))
+                throw new Exception("MediaCategory is missing 'description' data");
+        }
+
+        private List<MediaCategory> CreateMediaCategory(SqlDataReader reader)
+        { 
+            List<MediaCategory> returnMediaCategories = new List<MediaCategory>();
+
+            while (reader.Read())
+            { 
+                int id = reader.GetInt32(reader.GetOrdinal("id"));
+                string name = reader.GetString(reader.GetOrdinal("name"));
+                string description= reader.GetString(reader.GetOrdinal("description"));
+
+                //TODO userdata has to be fetched witht he rast of the data
+                returnMediaCategories.Add(new MediaCategory(id, name, description));
+            }
+
+            return returnMediaCategories;
+        }
+
+        private MediaCategory[] ListToArray(List<MediaCategory> incList)
+        {
+            return incList.ToArray();
+        }
+         
+    }
+}
---
+++ RestService/RestService/Handlers/TagHandler.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using RestService.Security;
+using RestService.Entities;
+
+namespace RestService.Handlers
+{
+    class TagHandler : AbstractHandler<Tag>
+    {
+        public TagHandler (DatabaseConnection incDbCon, Permissions perm) : base(incDbCon, perm) { }
+
+        public override void Create(Dictionary<string, string> data)
+        {
+            Validate(data);
+
+            PreparedStatement stat = dbCon.Prepare("INSERT INTO tag (name, tag_group_id, simple_name) VALUES ('" + data["name"] + "','" + data["tag_group_id"] + "', '" + data["simple_name"] + "')");
+            
+            dbCon.Command(data, stat);
+        }
+
+        public override Tag[] Read(int id)
+        {
+            PreparedStatement stat = dbCon.Prepare("SELECT * FROM tag where id = '" + id + "'");
+
+            return ListToArray(CreateTag(dbCon.Query(new Dictionary<string, string>(), stat)));
+        }
+
+        public override void Update(int id, Dictionary<string, string> data)
+        {
+            Validate(data);
+
+            PreparedStatement stat = dbCon.Prepare("UPDATE tag (name, simple_name)" +
+            "VALUES ('" + data["name"] + "','" + data["tag_group_id"] + "', '" + data["simple_name"] + "')");
+            dbCon.Command(data, stat);
+        }
+
+        public override void Delete(int id)
+        {
+            PreparedStatement stat = dbCon.Prepare("DELETE FROM tag where id = '" + id + "'");
+
+            dbCon.Command(new Dictionary<string, string>(), stat);
+        }
+
+        public override Tag[] Search(Dictionary<string, string> data)
+        {
+            string searchParams = "";
+
+            if (data.Count != 0)
+            {
+                searchParams += " where ";
+
+                foreach (KeyValuePair<string, string> s in data)
+                {
+                    string semiResult = s.Key + " = '" + s.Value + "' and ";
+                    searchParams += semiResult;
+                }
+
+                // removes the last "and" since there are no more params to search for
+                searchParams = searchParams.Remove(searchParams.Length - 4);
+            }
+
+            PreparedStatement stat = dbCon.Prepare("SELECT * FROM tag" + searchParams);
+
+            return ListToArray(CreateTag(dbCon.Query(data, stat)));
+        }
+
+        
+        public override void Validate(Dictionary<string, string> data)
+        {
+            if (!data.ContainsKey("name"))
+                throw new Exception("Tag is missing 'name' data");
+            if (!data.ContainsKey("tag_group_id"))
+                throw new Exception("Tag is missing 'name' data");
+            if (!data.ContainsKey("simple_name"))
+                throw new Exception("Tag is missing 'simple_name' data");
+        }
+
+        private List<Tag> CreateTag(SqlDataReader reader)
+        {
+            List<Tag> returnTags = new List<Tag>();
+
+            while (reader.Read())
+            { 
+                int id = reader.GetInt32(reader.GetOrdinal("id"));
+                int tag_group_id = reader.GetInt32(reader.GetOrdinal("tag_group_id"));
+                string name = reader.GetString(reader.GetOrdinal("name"));
+                string simple_name= reader.GetString(reader.GetOrdinal("simple_name"));
+
+                returnTags.Add(new Tag(id, tag_group_id, name, simple_name));
+            }
+
+            return returnTags;
+        }
+
+        private Tag[] ListToArray(List<Tag> incList)
+        {
+            return incList.ToArray();
+        }
+    }
+}
